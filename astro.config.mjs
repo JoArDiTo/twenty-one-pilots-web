2a7c3ea6c604f2d6f6346529a5a051d6022ade0b
JoArDiTo <sm.armando05@gmail.com>
2024-09-01 00:55:07 -0500

    Configure Astro deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  output: 'server',
+  site: 'https://JoArDiTo.github.io/twenty-one-pilots-web',
+  base: '/twenty-one-pilots-web',
 });
