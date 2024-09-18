2fa33f9c0dc6f104cc9fceb7c4c5776a6d6f1b00
JoArDiTo <sm.armando05@gmail.com>
2024-09-18 12:29:58 -0500

    output changed to static for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
+  output: 'static',
   site: 'https://JoArDiTo.github.io/twenty-one-pilots-web',
   base: '/',
 });
